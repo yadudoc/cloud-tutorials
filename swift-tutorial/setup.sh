47ebbf6d06483bec8c0e95171c788d6003983c69
Ubuntu <ubuntu@ip-172-31-37-216.us-west-2.compute.internal>
2014-09-26 18:21:39 +0000

    Minor update

--- swift-tutorial/setup.sh
+++ swift-tutorial/setup.sh
@@ -14,7 +14,7 @@ else
   echo Assuming $TUTDIR/bin:$TUTDIR/app: is already at front of PATH
 fi
 
-if [[ $HOSTNAME == headnode* ]]
+if [ -d /usr/local/bin/swift-trunk ] && [ -d /usr/local/bin/jdk1.7.0_51 ]
 then
     export JAVA=/usr/local/bin/jdk1.7.0_51/bin
     export SWIFT=/usr/local/bin/swift-trunk/bin
